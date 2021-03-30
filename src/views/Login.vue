e95400cccde4b938767fac674701cd2cc14453f3
Andre Winkler <gluehloch@googlemail.com>
2021-03-30 06:35:02 +0200

    Updated eslint (no padded blocks). Added simple login form.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
     <h1>This is the login/logout page</h1>
+    <form id="loginform" name="loginform">
+        <input type="text" id="username" name="username" max="50"/>
+        <input type="password" id="password" name="password"/>
+        <button id="loginbutton" name="loginbutton" v-on:click="startLogin" title="Login">Login</button>
+        <button id="resetbutton" name="resetbutton" v-on:click="reset" title="Reset">Reset</button>
+    </form>
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class Login extends Vue {}
+export default class Login extends Vue {
+
+  startLogin(): void {
+    console.log('Login :: startLogin')
+  }
+
+  reset(): void {
+    console.log('Login :: reset')
+  }
+
+}
 </script>
