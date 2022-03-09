8ff20335fee91ae24f89b1ec6a8ba73fc8ee9196
gluehloch <gluehloch@googlemail.com>
2022-03-09 19:56:10 +0100

    WIP: Vue-Bootstrap-Components.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import vSelect from 'vue-select';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-select/dist/vue-select.css';
 
 Vue.config.productionTip = false
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.component('v-select', vSelect);
 
